df2845e67afd3745f79b20c31a81f8dbcf08b60a
daliborstakic <stakicdalibor64@gmail.com>
2020-09-05 15:05:06 +0200

    the numbers are now drawn in the grid

--- sudoku.py
+++ sudoku.py
@@ -1,21 +1,21 @@
 """ Importing pygame """
+from turtle import width
 import pygame
 
+pygame.init()
+
 class Cell():
-    def __init__(self, value, color, row, col):
-        self._value = value
+    def __init__(self, number, color, row, col):
+        self._number = number
         self._color = color
-        self.row = row
-        self.col = col
-        self.font = pygame.font.SysFont('Arial', 20)
 
     @property
-    def value(self):
-        return self._value
+    def number(self):
+        return self._number
 
-    @value.setter
-    def value(self, value):
-        self._value = value
+    @number.setter
+    def number(self, number):
+        self._number = number
 
     @property
     def color(self):
@@ -25,17 +25,9 @@ class Cell():
     def color(self, color):
         self._color = color
 
-    def draw_number(self, win, width, rows):
-        gap = width // rows
-        x = self.row * gap
-        y = self.col * gap
-
-        text = self.font.render(str(self.value), 1, self.color)
-        text.blit(win, (x + text.get_width() / 2, y + text.get_height()))
-
 
 # Global parameters
-WIDTH = 500
+WIDTH = 450
 ROWS = 9
 
 # Colors
@@ -44,6 +36,9 @@ BLACK = (39, 39, 39)
 GRAY = (145, 145, 145)
 RED = (234, 35, 36)
 GREEN = (80, 197, 85)
+
+# Number font
+NUMBER_FONT = pygame.font.SysFont('Arial', 50)
 
 # Screen
 win = pygame.display.set_mode((WIDTH, WIDTH))
@@ -55,7 +50,7 @@ def gen_empty_grid(rows):
     for i in range(rows):
         grid.append([])
         for j in range(rows):
-            grid[i].append(0)
+            grid[i].append(Cell(0, BLACK, i, j))
     
     return grid
 
@@ -68,7 +63,7 @@ def find_empty(grid):
     """ Finds an unfilled cell """
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if grid[i][j] == 0:
+            if grid[i][j].number == 0:
                 return i, j
 
 def is_valid(grid, num, position):
@@ -77,13 +72,13 @@ def is_valid(grid, num, position):
 
     # Checking the horizontal rows
     for i in range(len(grid[0])):
-        if grid[row][i] == num and col != i:
+        if grid[row][i].number == num and col != i:
             return False
 
     # Checking the vertical columns
     for i in range(len(grid)):
-        if grid[i][col] == num and row != i:
-            return False
+        if grid[i][col].number == num and row != i:
+            return False    
 
     # Checking the 3X3 boxes
     box_x = col // 3
@@ -91,16 +86,14 @@ def is_valid(grid, num, position):
 
     for i in range(box_y * 3, box_y * 3 + 3):
         for j in range(box_x * 3, box_x * 3 + 3):
-            if grid[i][j] == num and (i, j) != position:
+            if grid[i][j].number == num and (i, j) != position:
                 return False
 
     return True
 
-def solve(grid, width, rows, draw=None, is_drawn=None):
+def solve(grid):
     """ Solves an empty sudoku board """
     find = find_empty(grid)
-
-    draw()
 
     if not find: # If it doesn't find an empty cell
         return True
@@ -109,14 +102,15 @@ def solve(grid, width, rows, draw=None, is_drawn=None):
 
     for i in range(1, 10):
         if is_valid(grid, i, (row, col)):
-            grid[row][col] = i # Sets value
+            grid[row][col].number = i # Sets number
 
             if solve(grid): # Recursive call
                 return True
 
-            grid[row][col] = 0
+            grid[row][col].number = 0
 
 def draw_grid(win, width, rows):
+    """ Draws the grid lines """
     gap = width // rows
 
     for i in range(rows):
@@ -128,15 +122,32 @@ def draw_grid(win, width, rows):
             y_color = BLACK if j % 3 == 0 else GRAY
             pygame.draw.line(win, y_color, (0, y), (width, y))
 
-def draw(win, width, rows):
+def draw(win, grid, width, rows):
     """ Drawing and rendering the screen """
     win.fill(WHITE)
     draw_grid(win, width, rows)
+    
+    gap = width // rows
+
+    """ Drawing the numbers """
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            text = NUMBER_FONT.render(str(grid[i][j].number), 1, grid[i][j].color)
+            
+            x = i * gap
+            y = j * gap
+
+            if grid[i][j].number != 0:
+                win.blit(text, (x + 10, y - 3))
+
     pygame.display.update()
 
 def main():
     """ Main function """
     run = True
+
+    grid = gen_empty_grid(ROWS)
+    solve(grid)
 
     # Main loop
     while run:
@@ -144,7 +155,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        draw(win, WIDTH, ROWS)
+        draw(win, grid, WIDTH, ROWS)
 
     pygame.quit()
 
